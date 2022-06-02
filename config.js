c19cef60509852dbc066f66329f0bdeadc3d41c1
phngleena <106763451+phngleena@users.noreply.github.com>
2022-06-02 23:30:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    titleWeb: "Hế lô",
+    introTitle: 'Gửi bạn thân iu',
+    introDesc: `Bấm zô cái nút ở dứoi ó, thấy hok ????`,
+    btnIntro: 'nè nè nó nè',
+    title: 'Iu tớ đúng hok :>?',
+    desc: 'hok trả lời là tớ cạp đầu, out ra là iu tớ :33',
+    btnYes: 'Phương Linh số 1 💕',
+    btnNo: 'hok bé uiiii',
+    question: 'tớ bíc cậu cũng iu tớ mà',
+    btnReply: 'Gửi',
+    reply: '',
+    mess: 'hok nói nhìu, nói chung là cậu iu tớ',
+    messDesc: 'tớ cũng iu cậu hihi 🙆‍♀️',
+    btnAccept: 'oki iu cậu ',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
